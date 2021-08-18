4e09e25a0741613a032cd63687891fb935f9370f
lekoal <coreglus@gmail.com>
2021-08-18 15:46:52 +0300

    Class Animal

---
+++ src/ru/geekbrains/java_core_homework_6/Animal.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java_core_homework_6;
+
+public class Animal {
+
+    public void run(int length) {
+        System.out.printf("Животное пробежало %s метров%n", length);
+    }
+
+    public void swim(int length) {
+        System.out.printf("Животное проплыло %s метров%n", length);
+    }
+
+}
